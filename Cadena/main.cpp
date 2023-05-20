4653170cd520442f8eb77231b9472b40db92ef9d
Jesica Fernandez <je.fernandez.cai@gmail.com>
2023-05-20 13:04:24 -0300

    Update main.cpp

--- Cadena/main.cpp
+++ Cadena/main.cpp
@@ -8,12 +8,12 @@ int main()
     Cadena c1("Hola");
     cout << "c1: " << c1 << endl;
 
-    Cadena c2(c1); /// por esto tambien necesito constructor de copia??
+    Cadena c2(c1);
     cout << "c2: " << c2 << endl;
 
     Cadena c3;          /// en memoria dinamica debo definir el constructor de copia cuando un operador devuelve un nuevo objeto
     c3 = c1 + " Mundo"; /// en memoria dinamica debo definir el operador de asignacion(=)
-    cout << "c3: " << c3 << endl; /// el constructor de copia aca se ejecuta despues de la suma y antes del igual
+    cout << "c3: " << c3 << endl;
 
     cout << "Resumen:" << endl;
     cout << "c1: " << c1 << endl;
